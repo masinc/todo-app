07233715fb2218623310c7d18bd467b2715c9937
masinc <masinc000@gmail.com>
2022-12-06 05:43:21 +0900

    add Docker

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -33,7 +33,7 @@ async fn main() -> anyhow::Result<()> {
             .route("/", web::get().to(HttpResponse::Ok))
             .configure(|cfg| api::init(cfg).unwrap())
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await?;
 
